aaadd573b70f1bb13eb14ede3d6076052a1c710a
bobosGS <54885612+bobosGS@users.noreply.github.com>
2025-11-10 21:51:54 +0100

    Update defaultPins.h

--- Software/rf_test/defaultPins.h
+++ Software/rf_test/defaultPins.h
@@ -24,8 +24,8 @@
 #else
     // pins available on (ADAFRUIT_ITSYBITSY_RP2040 || ADAFRUIT_FEATHER_RP2040 || Pico_board || Sparkfun_ProMicro || SparkFun MicroMod)
 
-    #define CE_PIN  7
-    #define CSN_PIN 8
+    #define CE_PIN  2
+    #define CSN_PIN 3
     #define IRQ_PIN 6
 #endif // board detection macro defs
 
